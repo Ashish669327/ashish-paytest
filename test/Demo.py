083ca8df5293b7a0632e06063a962d58c299ccba
ashishsaklani <ashishsaklani008@gmail.com>
2025-03-26 16:16:14 +0530

    commit new

---
+++ test/Demo.py
@@ -0,0 +1,10 @@
+class Demo:
+    def add(self, a, b):
+        result = a + b
+        print(f"The sum of {a} and {b} is: {result}")
+
+# Create an instance of the class
+calc = Demo()
+
+# Call the add function
+calc.add(5, 3)
